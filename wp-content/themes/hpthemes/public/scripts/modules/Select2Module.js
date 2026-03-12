8c6d4b90a9631169f43209950576d89b0028f8f5
Phat Luong <phat.luong@hdwebsoft.dev>
2026-03-12 14:27:29 +0700

    Update CI/CL ai

--- wp-content/themes/hpthemes/public/scripts/modules/Select2Module.js
+++
@@ -1,9 +0,0 @@
-export default function Select2Module() {
-  $(document).ready(function () {
-    if ($(".re-select-main").length > 0) {
-      $(".re-select-main").select2();
-    }
-  });
-}
-
-Select2Module();
