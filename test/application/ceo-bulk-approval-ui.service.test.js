d453860a975a0b7688cc8f3f6886bcb9e4cfd79e
PAT <pat@test.com>
2026-07-25 17:29:54 +0700

    feat(T193): implement CEO bulk approval UI service

---
+++ test/application/ceo-bulk-approval-ui.service.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const CeoBulkApprovalUiService = require('../../src/application/services/ceo-bulk-approval-ui.service');
+
+describe('T193: CEO Bulk Approval UI Service', () => {
+  test('processes bulk approval UI action with filtering, re-authentication, and per-item results', () => {
+    const service = new CeoBulkApprovalUiService({});
+    const res = service.processBulkApprovalUi({ batchId: 'b_100', selectedItemIds: ['item_1', 'item_2'] });
+
+    expect(res.status).toBe('BULK_APPROVED_SUCCESS');
+    expect(res.reauthenticated).toBe(true);
+    expect(res.perItemResults.length).toBe(2);
+  });
+});
