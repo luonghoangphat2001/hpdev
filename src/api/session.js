d4899af7b25ffb7ba395524039c4d1a10fe38f0c
PAT <pat@test.com>
2026-08-23 14:25:02 +0700

    test deploy manager

--- src/api/session.js
+++ src/api/session.js
@@ -3,5 +3,5 @@ import { api } from './request';
 export const login = (username, password) => api.post('/login', { username, password });
 export const logout = () => api.post('/logout');
 export const getMe = () => api.get('/me');
-export const changePassword = (currentPassword, newPassword) => 
+export const changePassword = (currentPassword, newPassword) =>
   api.post('/password', { currentPassword, newPassword });
